c89f9f9adbecad9a0386e821bb6e61c3ee2c286a
BrunoSNSantos <techbruno.santos@gmail.com>
2024-06-17 16:50:51 -0300

    Novos estudos e criação do sistema de uma SmartTv

--- TIPOS-VARIAVEIS/src/Operadores.java
+++ TIPOS-VARIAVEIS/src/Operadores.java
@@ -22,6 +22,8 @@ public class Operadores {
         
         System.out.println(variavel);
 
+
+
         //Exemplo de estrutura condicional usual: 
         int a, b; 
 
@@ -49,10 +51,36 @@ public class Operadores {
         
         System.out.println(result);
 
-        //Operadores Relacionais:
-        
-       
 
+
+        //Operadores Relacionais:
+        // == quando desejamos vericiar se dois valores são iguais 
+        // != verifica diferença entre dpois algarismos 
+        // > maior que
+        // >= maior ou igual
+        // < menor que
+        // <= menor ou igual 
+
+        String nomeUm = "Bruno";
+        String nomeDois = new String("Bruno");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        //para objetos é bom usar .equals 
+        //para números pode usar operadores relacionais 
+
+
+        // Operadores Lógicos: 
+        // && - Operador lógico "e"
+        // || - Operador lógico "ou"
+
+        boolean condicaoUm = true;
+        boolean condicaoDois = false;
+
+        if (condicaoUm || condicaoDois){
+            System.out.println("As duas condições são verdadeiras ");
+        }
+    
     }
     
 }
