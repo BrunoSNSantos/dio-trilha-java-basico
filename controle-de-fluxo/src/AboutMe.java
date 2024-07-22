dd069942875686727b81370c3826447399057e14
BrunoSNSantos <techbruno.santos@gmail.com>
2024-07-22 16:52:14 -0300

    Estudos sobre trat5amento de exceções

--- controle-de-fluxo/src/AboutMe.java
+++ controle-de-fluxo/src/AboutMe.java
@@ -1,21 +1,23 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
         //criando o objeto scanner
+        try{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome:");
+        System.out.println("Digite seu nome");
         String nome = scanner.next();
 
-        System.out.println("Digite seu sobrenome:");
+        System.out.println("Digite seu sobrenome");
         String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade:");
+        System.out.println("Digite sua idade");
         int idade = scanner.nextInt();
 
-        System.out.println("Digite sua altura:");
+        System.out.println("Digite sua altura");
         double altura = scanner.nextDouble();
 
 
@@ -23,6 +25,10 @@ public class AboutMe {
         System.out.println("Tenho " + idade + " anos");
         System.out.println("Minha altura é " + altura + "cm ");
         scanner.close();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Os campos idade e altura precisam ser numéricos");
+        }
 
     }    
 }
